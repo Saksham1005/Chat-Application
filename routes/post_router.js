c12cf2f0aff0b9e91d225ed86930f82881d9d342
unknown <Saksham1005@gmail.com>
2022-01-07 23:23:40 +0530

    Added two layer security for creating posts

--- routes/post_router.js
+++ routes/post_router.js
@@ -1,8 +1,10 @@
 const express=require("express")
 const router=express.Router()
+const passport=require("passport")
+const passportLocal=require("../config/passport")
 
 const post_controller=require("../controller/post_controller")
 
-router.post("/post/create",post_controller.create_post)
+router.post("/post/create",passportLocal.checkAuthentication,post_controller.create_post)
 
 module.exports=router;
