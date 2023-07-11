24346395aa7e1b67a2f8220ca86633dff16589fd
Saksham1005 <Saksham1005@gmail.com>
2023-07-11 12:52:32 +0530

    Updating View Helpers.

--- config/view-helpers.js
+++ config/view-helpers.js
@@ -1,15 +1,24 @@
-const env = require('./environment');
-const fs = require('fs');
-const path = require('path');
+const env = require("./environment");
+const fs = require("fs");
+const path = require("path");
 
-// Making a global function to be used in the views so as to extract file name of different 
+// Making a global function to be used in the views so as to extract file name of different
 // css , js , images files (if exist) to our view
 module.exports = (app) => {
-    app.locals.assetPath = function(filePath){
-        if (env.name == 'development'){
-            return filePath;
-        }
-
-        return '/' + JSON.parse(fs.readFileSync(path.join(__dirname, '../public/assets/rev-manifest.json')))[filePath];
+  app.locals.assetPath = function (filePath) {
+    if (env.name == "development") {
+      return filePath;
     }
-}
+
+    return filePath;
+
+    return (
+      "/" +
+      JSON.parse(
+        fs.readFileSync(
+          path.join(__dirname, "../public/assets/rev-manifest.json")
+        )
+      )[filePath]
+    );
+  };
+};
